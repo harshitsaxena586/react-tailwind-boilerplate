69ca8da0d302bee8fbb35461896ff9fb217d1b4d
harshit saxena <harshitsaxena586@gmail.com>
2021-06-28 02:33:21 +0530

    intial commit

---
+++ subs/frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center mt-60 text-8xl font-semibold">
+        Start building
+      </div>
+    </div>
+  );
+}
+
+export default App;
